d620dc877fd858fb191566307ccb34af71f38ff8
Jeff Langdon <jeff.beyer@gmail.com>
2024-12-18 21:48:52 -0800

    fix lints

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -3,27 +3,27 @@ import { getAPIKey } from "src/api/auth";
 import { IncomingHttpHeaders } from "http";
 
 test("no header", () => {
-  let header: IncomingHttpHeaders = {};
+  const header: IncomingHttpHeaders = {};
 
   expect(getAPIKey(header)).toBeNull();
 });
 
 test("bad key def", () => {
-  let header: IncomingHttpHeaders = { authorization: "NotApiKey" };
+  const header: IncomingHttpHeaders = { authorization: "NotApiKey" };
 
   console.log(header.authorization);
   expect(getAPIKey(header)).toBeNull();
 });
 
 test("bad key def", () => {
-  let header: IncomingHttpHeaders = { authorization: "ApiKey" };
+  const header: IncomingHttpHeaders = { authorization: "ApiKey" };
 
   console.log(header.authorization);
   expect(getAPIKey(header)).toBeNull();
 });
 
 test("bad key def", () => {
-  let header: IncomingHttpHeaders = { authorization: "ApiKey myKeySux" };
+  const header: IncomingHttpHeaders = { authorization: "ApiKey myKeySux" };
 
   console.log(header.authorization);
   expect(getAPIKey(header)).toBe("myKeySux");
