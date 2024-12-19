5827a733a75c142d3a48a70a473c8923ce8f3718
Jeff Langdon <jeff.beyer@gmail.com>
2024-12-18 21:59:15 -0800

    adding security check (should fail)

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,17 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import pluginSecurity from "eslint-plugin-security";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    languageOptions: {
+      globals: { window: true, console: true, document: true },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
 ];
+
