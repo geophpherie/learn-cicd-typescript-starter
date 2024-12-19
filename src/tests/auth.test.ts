dc50a9953cd1943bc461a4911b8555258270ac77
Jeff Langdon <jeff.beyer@gmail.com>
2024-12-18 21:28:07 -0800

    adding a broken test with ci

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,30 @@
+import { expect, test } from 'vitest'
+import { getAPIKey } from 'src/api/auth'
+import { IncomingHttpHeaders } from "http"
+
+test("no header", () => {
+  let header: IncomingHttpHeaders = {}
+
+  expect(getAPIKey(header)).toBeNull()
+})
+
+test("bad key def", () => {
+  let header: IncomingHttpHeaders = { authorization: "NotApiKey" }
+
+  console.log(header.authorization)
+  expect(getAPIKey(header)).toBeNull()
+})
+
+test("bad key def", () => {
+  let header: IncomingHttpHeaders = { authorization: "ApiKey" }
+
+  console.log(header.authorization)
+  expect(getAPIKey(header)).toBeNull()
+})
+
+test("bad key def", () => {
+  let header: IncomingHttpHeaders = { authorization: "ApiKey myKeySux" }
+
+  console.log(header.authorization)
+  expect(getAPIKey(header)).toBe("myKeySu")
+})
